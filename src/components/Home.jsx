7814186056e4d758c60b407ef5ba2fa287012b28
Amit Verma <amitver6969@gmail.com>
2024-05-29 10:58:41 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
       <div className="flex flex-col lg:flex-row justify-center gap-10 font-bold font-sans bg-zinc-800 m-20">
         <Link to="/sign" className="hover:opacity-80">
           <div className="w-full max-w-md lg:w-1/2 md:w-3/4 sm:w-full bg-zinc-600 text-white shadow-lg font-mono rounded-lg border-black border-solid p-6 text-center mx-auto">
-            <h2 className="bg-zinc-600 p-14 text-lg md:text-2xl lg:text-3xl">SIGN HERE </h2>
+            <h2 className="bg-zinc-600 flex justify-center p-14 text-lg md:text-2xl lg:text-3xl">SIGN HERE </h2>
             <h4 className='bg-zinc-600 text-sm md:text-base lg:text-lg'>use your TOUCHPAD, MOBILE SCREEN to draw your signature</h4>
           </div>
         </Link>
